359d270182988133ef8a2127f76d95720e3c9e1a
Nabeel Maredia <maredian8@gator.uhd.edu>
2020-02-25 16:56:03 -0600

    added random stuff

--- Demo/new-window.vb
+++ Demo/new-window.vb
@@ -1,5 +1,6 @@
 ﻿Public Class new_window
     Private Sub Exitbtn_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
         Application.Exit()
+        MsgBox("sdfaddsgfg")
     End Sub
 End Class
