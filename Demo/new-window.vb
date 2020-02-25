404f3fb619b4522a948fbef2b7f66671cb30ff45
Nabeel Maredia <maredian8@gator.uhd.edu>
2020-02-25 16:49:24 -0600

    exit button added

--- Demo/new-window.vb
+++ Demo/new-window.vb
@@ -1,5 +1,5 @@
 ﻿Public Class new_window
     Private Sub Exitbtn_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
-        MsgBox("R U sure?")
+        Application.Exit()
     End Sub
 End Class
