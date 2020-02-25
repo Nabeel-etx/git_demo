27a5e527729c7fb50fa5b94736ba5bed6428c09a
Nabeel Maredia <maredian8@gator.uhd.edu>
2020-02-25 16:58:49 -0600

    bla-bla

--- Demo/new-window.vb
+++ Demo/new-window.vb
@@ -1,6 +1,5 @@
 ﻿Public Class new_window
     Private Sub Exitbtn_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
         Application.Exit()
-        MsgBox("sdfaddsgfg")
     End Sub
 End Class
