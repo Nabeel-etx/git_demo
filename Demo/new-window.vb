a3fb56648176d571fcae0d94a9b536bba3540d2f
Nabeel <57419564+Nabeel-etx@users.noreply.github.com>
2020-02-25 16:45:18 -0600

    Update new-window.vb

--- Demo/new-window.vb
+++ Demo/new-window.vb
@@ -1,5 +1,5 @@
 ﻿Public Class new_window
     Private Sub Exitbtn_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
-        Application.Exit()
+        MsgBox("R U sure?")
     End Sub
 End Class
